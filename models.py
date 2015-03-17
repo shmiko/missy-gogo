18dd0d2db310957946c16399a1b171fb9b5de43e
Paul Jones <shmiko@gmail.com>
2015-03-17 19:58:37 +1100

    2

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+#from dist import google.appengine.ext 
 from google.appengine.ext import db
 
 class PythonTerm(db.Model):
@@ -8,3 +9,74 @@ class PythonTerm(db.Model):
 def store_pythonterm(query, result):
 		pythonterm = PythonTerm(term=query, definition=result)
 		pythonterm.put()
+
+
+#!/usr/bin/python2.5
+# Copyright 2009 Google Inc.
+#
+
+"""Sample Datastore models."""
+
+import datetime
+
+from google.appengine.ext import db
+from google.appengine.ext.db import polymodel
+
+# Models
+
+class Customer(db.Model):
+  """Very simple Customer class.
+
+  Uses named keys.
+  """
+  name = db.StringProperty()
+  phone = db.PhoneNumberProperty()
+  created = db.DateTimeProperty()
+
+
+class Visit(db.Model):
+  """Very simple customer visit class."""
+  customer = db.ReferenceProperty(Customer)
+  visit_date = db.DateTimeProperty()
+  score = db.FloatProperty()
+  activities = db.ListProperty(str)
+
+
+class VisitActivity(db.Model):
+  """An alternate way to represent activites.
+
+  This uses a child entity instead of listproperty. You probably
+  shouldn't do this for such a simple case, it's far less efficient.
+  
+  This exists as a demo for how you might handle a more complex entity group.
+
+  Key is ('Visit', visit id, 'VisitActivity', activity). We'll duplicate
+  activity as a property too even though that's unnecessary.
+  """
+  actvity = db.StringProperty()
+
+
+class CustomerCall(db.Model):
+  """Another very simple example class."""
+  customer = db.ReferenceProperty(Customer)
+  call_date = db.DateTimeProperty()
+  answered = db.BooleanProperty()
+
+
+class Attachment(db.Model):
+  filename = db.StringProperty()
+  filecontents = db.BlobProperty()
+
+# Polymodel
+# http://groups.google.com/group/google-appengine-python/browse_thread/thread/d92179c712bda721
+
+class Badge(db.Model):
+  name = db.StringProperty()
+  level = db.IntegerProperty()
+
+class Person(polymodel.PolyModel):
+  first_name = db.StringProperty()
+  last_name = db.StringProperty()
+
+class Adult(Person):
+  interests = db.StringListProperty()
--- term_loader.py
+++ term_loader.py
@@ -1,6 +1,16 @@
+from google.appengine.api import users
 from google.appengine.ext import db
 from google.appengine.tools import bulkloader
-from models import PythonTerm
+#from models import PythonTerm
+
+class PythonTerm(db.Model):
+	"""Models a term from the Python glossary."""
+	term = db.StringProperty()
+	definition = db.TextProperty()
+
+def store_pythonterm(query, result):
+		pythonterm = PythonTerm(term=query, definition=result)
+		pythonterm.put()
 
 class PythonTermLoader(bulkloader.Loader):
 	def __init__(self):
@@ -9,3 +19,10 @@ class PythonTermLoader(bulkloader.Loader):
 			('definition', str)
 			])
 loaders = [PythonTermLoader]
+
+
+appcfg.py upload_data --config_file="bulkloader.yaml" --filename="function_dict.csv" --url=http://missy-go.appspot.com/remote_api --application=missy-go --kind=PythonTerm --email=shmiko@gmail.com --passin
+
+#appcfg.py upload_data --url=http://127.0.0.1:8088/remote_api --filename=function_dict.csv --has_header --application=dev~missy-go --kind=PythonTerm
+appcfg.py upload_data --config_file="bulkloader.yaml" --filename="function_dict.csv" --url=http://missy-go.appspot.com:8088/remote_api --application=missy-go --kind=PythonTerm --email=shmiko@gmail.com --passin
+appcfg.py upload_data --application=missy-go --config_file="bulkloaderb.yaml" --filename="customer.csv" --kind=Customer --url=http://missy-go.appspot.com:8080/remote_api
